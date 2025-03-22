1a4c2a37cf3225e1246f35f635e9393f571838df
ntua-el20227 <koustenisch@gmail.com>
2025-03-22 14:08:02 +0200

    reverse Integer added

--- 3Sum/3Sum.py
+++ 3Sum/3Sum.py
@@ -1,29 +1,34 @@
-from typing import List
-
-
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
-        nums.sort()
+        nums.sort() # O (n log n)
         n = len(nums)
+        result = []
+        # Dictionary to store the triplet.
+        m = set()
         # Initialize closest_sum with the sum of the first three elements.
-        closest_sum = nums[0] + nums[1] + nums[2]
-        
         # Iterate over the list, taking one number at a time.
-        for i in range(n - 2):
+        for i in range(n):
+            if (i > 0) and (nums[i] == nums[i-1]):
+                continue
             left, right = i + 1, n - 1
             while left < right:
                 current_sum = nums[i] + nums[left] + nums[right]
-                # Update closest_sum if the current sum is closer to target.
-                if abs(target - current_sum) < abs(target - closest_sum):
-                    closest_sum = current_sum
-                
+                # Append triplet to result if current_sum equals 0.
+                if current_sum == 0:
+                    if (nums[i],nums[left],nums[right]) not in m:
+                        result.append([nums[i], nums[left], nums[right]])
+                        m.add((nums[i],nums[left],nums[right]))
                 # Move pointers based on how current_sum compares to target.
-                if current_sum < target:
+                if current_sum < 0:
                     left += 1
-                elif current_sum > target:
+                elif current_sum > 0: 
                     right -= 1
                 else:
-                    # If current_sum equals target, it's the closest possible.
-                    return current_sum
+                    left += 1
+                    right -= 1
         
-        return closest_sum
+        return result
+    
+# Time complexity: O(n^2)
+# Space complexity: O(n)
+
